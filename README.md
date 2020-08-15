23e285027dba587eaa1ca9d801b93ba55b0d5c8a
Claire Kolln <ckolln55@gmail.com>
2020-08-15 14:20:45 -0700

    Outline to process CSV

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # ms3-challenge
  MS3 Junior Coding Challenge Solution REPO
+
+# Set Environment variables
+
+- Set JAVA_HOME environment variable
+- Set CLASSPATH: 
+
+`export CLASSPATH=.:./dependency/sqlite-jdbc-3.30.1.jar:./dependency/commons-csv-1.8.jar`
+
+# Compilation and Running
+
+`javac MS3.java`
+`java MS3 <input file name>`
--- ms3-app/MS3.java
+++ ms3-app/MS3.java
@@ -1,15 +1,51 @@
 import java.sql.*;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.io.FileReader;
+
 
 public class MS3 {
       public static void main( String args[] ) {
-          Connection c = null;
-          try {
-             Class.forName("org.sqlite.JDBC");
-             c = DriverManager.getConnection("jdbc:sqlite:test.db");
-          } catch ( Exception e ) {
-             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-             System.exit(0);
-          }
-          System.out.println("Opened database successfully");
+         try {
+            Reader in = new FileReader("./resources/ms3Interview.csv");
+            Iterable<CSVRecord> records = CSVFormat.EXCEL.withFirstRecordAsHeader().withIgnoreSurroundingSpaces().withNullString("").parse(in);
+            Integer counter = 0;
+            for (CSVRecord record : records) {
+               for (String cell : record) {
+                  System.out.println(cell);
+                  if (cell == null) {
+                     // add this record to the log
+                     // increase failure count
+                  }
+               }
+               // increase success count
+               
+               break;
+               // String A = record.get(0);
+               // System.out.println(A==null);
+               // System.out.println(A);
+               // counter = counter + 1;
+               
+
+            }
+         } catch (Exception e) {
+            System.err.println(e);
+         }
+         
+         
+         //  Connection c = null;
+         //  try {
+         //     Class.forName("org.sqlite.JDBC");
+         //     c = DriverManager.getConnection("jdbc:sqlite:test.db");
+         //  } catch ( Exception e ) {
+         //     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+         //     System.exit(0);
+         //  }
+         //  System.out.println("Opened database successfully");
        }
 }
