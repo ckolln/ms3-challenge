52e248c043217744632fbb2583c254f1b0c528b7
Claire Kolln <ckolln55@gmail.com>
2020-08-16 14:10:37 -0700

    Final Push

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # ms3-challenge
- MS3 Junior Coding Challenge Solution REPO
+ MS3 Junior Coding Challenge Solution REPO. 
+ Assignment is to consume a large csv file, add complete records to a sqlite database, add the incomplete records to a csv file, and to log the number of success and failures.
 
 # Set Environment variables
 
@@ -8,7 +9,37 @@
 
 `export CLASSPATH=.:./dependency/sqlite-jdbc-3.30.1.jar:./dependency/commons-csv-1.8.jar`
 
+# Directory Structure
+
+All of the resources (csv files, database files, and logs) will be stored in the resources directory. The .jar files for the dependences (Apache Commons CSV and SQLITE JDBC) are located in the /dependency directory.
+
 # Compilation and Running
+
+After cloning this repo, from the ms3-app directory: 
 
 `javac MS3.java`
 `java MS3 <input file name>`
+
+Example: 
+
+`java MS3 ./resources/ms3Interview.csv`
+
+# Approach and Design
+
+Frameworks used: Apache Commons CSV, SQLite JDBC
+
+I chose to use a framework to help consume the csv file because it is a more streamlined approach to working with the files. 
+
+I completed the project by the following steps: 
+
+1. I first set up my environment - including CLASSPATH and JAVA_HOME variables
+2. I tested the sqlite jdbc framework by opening a test db. 
+3. Commenting out the sqlite code, I tested the Apache Commons CSV framework to consume the first line of the test file. I chose to look at the line by itself, then to go through each cell to make sure that it was not a `null` value. 
+4. I wrote the entire outline for the functionalities I knew I would need: adding bad records to the csv, adding complete records to the database, opening the database and creating the table, and creating the log file. While I filled in these functions later, it was important for me to map out where each function would occur and how they would all connect. 
+5. After I was sure that both frameworks were working well independently, I combined the two methods to add the complete records to a database. First for 50 lines, then 500, then continuing in this way to test the approach thoroughly. 
+6. After I was sure the database was being updated, I implemented the writing to a csv file for the incomplete records. 
+7. Finally I did the functionality to write to the log file. 
+
+# Author
+
+Claire Kolln, ckolln55@gmail.com
--- ms3-app/MS3.java
+++ ms3-app/MS3.java
@@ -1,51 +1,148 @@
 import java.sql.*;
-import org.apache.commons.csv.CSVFormat;
-import org.apache.commons.csv.CSVParser;
-import org.apache.commons.csv.CSVRecord;
+import org.apache.commons.csv.*;
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.logging.*;
+
 
 
 public class MS3 {
-      public static void main( String args[] ) {
+
+      // creates database with name <insert-filename>.db
+      private Connection connect(String name) {
+         Connection c = null;
+         String url = "jdbc:sqlite:"+name+".db";
          try {
-            Reader in = new FileReader("./resources/ms3Interview.csv");
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection(url);
+         } catch (Exception e) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+         }
+         createNewTable(name);
+         return c;
+      }
+
+      // create new table inside <insert-filename>.db
+      public static void createNewTable(String name) {
+         // SQLite connection string
+         String url = "jdbc:sqlite:"+name+".db";
+         
+         // SQL statement for creating a new table
+         String sql = "CREATE TABLE IF NOT EXISTS ms3 (\n"
+                 + "	A text,\n"
+                 + "	B text,\n"
+                 + "	C text PRIMARY KEY,\n"
+                 + "	D text,\n"
+                 + "	E text,\n"
+                 + "	F text,\n"
+                 + "	G text,\n"
+                 + "	H text,\n"
+                 + "	I text,\n"
+                 + "	J text\n"
+                 + ");";
+         
+         try (Connection conn = DriverManager.getConnection(url);
+                 Statement stmt = conn.createStatement()) {
+             // create a new table
+             stmt.execute(sql);
+         } catch (SQLException e) {
+             System.out.println(e.getMessage());
+         }
+     }
+      
+      // writes bad record to csv file of name <insert-filename>-bad.csv
+      public void writeBadRecord(CSVRecord record, String fileName) {
+         try {
+            FileWriter writer = new FileWriter(fileName+"-bad.csv",true);
+            try(CSVPrinter printer = new CSVPrinter(writer, CSVFormat.RFC4180)) {
+               printer.printRecord(record);
+               printer.flush();
+            }
+         } catch (IOException e) {
+            e.printStackTrace();
+            }
+      }
+
+      // write log file to log of name <input-filename>.log
+      public void writeLogFile(Integer received, Integer success, Integer fail, String name) {
+         Logger ms3Logger = Logger.getLogger(name);
+         FileHandler ms3FileHandler; 
+         try {
+            ms3FileHandler = new FileHandler(name+".log", true);
+            ms3Logger.addHandler(ms3FileHandler);
+
+            SimpleFormatter formatter = new SimpleFormatter();	
+            ms3FileHandler.setFormatter(formatter);
+            ms3Logger.info("\nRecords Received - " + received + "\nSuccesses - " + success + "\nFailures - " + fail);
+         } catch (SecurityException e) {
+			e.printStackTrace();
+         } catch (IOException e) {
+            e.printStackTrace();
+         } 
+      }
+
+      // add record to database of name <input-filename>.db
+      public void addRecord(CSVRecord record, String fileName) {
+         String sql = "INSERT INTO ms3 (A,B,C,D,E,F,G,H,I,J) values (?,?,?,?,?,?,?,?,?,?)";
+         try (Connection conn = this.connect(fileName); PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            // add values to the prepared statement
+            stmt.setString(1,record.get(0));
+            stmt.setString(2,record.get(1));
+            stmt.setString(3,record.get(2));
+            stmt.setString(4,record.get(3));
+            stmt.setString(5,record.get(4));
+            stmt.setString(6,record.get(5));
+            stmt.setString(7,record.get(6));
+            stmt.setString(8,record.get(7));
+            stmt.setString(9,record.get(8));
+            stmt.setString(10,record.get(9));
+            stmt.executeUpdate();
+         }
+         catch (SQLException e) {
+            System.out.println(e.getMessage());
+         }
+      }
+
+      public static void main( String args[] ) {
+         
+         MS3 app = new MS3();
+         String fileName = args[0].substring(0,args[0].length() - 4);
+         int successCount = 0;
+         int failCount = 0; 
+         int receivedCount = 0;
+
+         try {
+            Reader in = new FileReader(args[0]);
             Iterable<CSVRecord> records = CSVFormat.EXCEL.withFirstRecordAsHeader().withIgnoreSurroundingSpaces().withNullString("").parse(in);
-            Integer counter = 0;
+            
             for (CSVRecord record : records) {
+               receivedCount++;
+               boolean insert = true;
                for (String cell : record) {
-                  System.out.println(cell);
                   if (cell == null) {
-                     // add this record to the log
-                     // increase failure count
+                     app.writeBadRecord(record,fileName);
+                     failCount++;
+                     insert = false;
+                     break;
                   }
                }
-               // increase success count
-               
-               break;
-               // String A = record.get(0);
-               // System.out.println(A==null);
-               // System.out.println(A);
-               // counter = counter + 1;
-               
-
+               if (insert) {
+                  successCount++;
+                  app.addRecord(record, fileName);
+               }
             }
          } catch (Exception e) {
+            
             System.err.println(e);
+         } finally {
+            app.writeLogFile(receivedCount,successCount, failCount, fileName);
          }
          
-         
-         //  Connection c = null;
-         //  try {
-         //     Class.forName("org.sqlite.JDBC");
-         //     c = DriverManager.getConnection("jdbc:sqlite:test.db");
-         //  } catch ( Exception e ) {
-         //     System.err.println( e.getClass().getName() + ": " + e.getMessage() );
-         //     System.exit(0);
-         //  }
-         //  System.out.println("Opened database successfully");
-       }
+      }
 }
